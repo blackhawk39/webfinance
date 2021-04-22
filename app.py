7e0d696d5c3e1024f053eac4f3dbbce33835db3d
sourabh <sourabh7146@gmail.com>
2021-04-22 14:43:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session, url_for
-from flask_session import Session
-from passlib.apps import custom_app_context as pwd_context
+from flask import Flask, flash, redirect, render_template, request, url_for
+#from passlib.apps import custom_app_context as pwd_context
 from tempfile import mkdtemp
 
 from helpers import *
@@ -10,22 +9,22 @@ from helpers import *
 app = Flask(__name__)
 
 # ensure responses aren't cached
-if app.config["DEBUG"]:
-    @app.after_request
-    def after_request(response):
-        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-        response.headers["Expires"] = 0
-        response.headers["Pragma"] = "no-cache"
-        return response
+# if app.config["DEBUG"]:
+#     @app.after_request
+#     def after_request(response):
+#         response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+#         response.headers["Expires"] = 0
+#         response.headers["Pragma"] = "no-cache"
+#         return response
 
-# custom filter
-app.jinja_env.filters["usd"] = usd
+# # custom filter
+# app.jinja_env.filters["usd"] = usd
 
-# configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+# # configure session to use filesystem (instead of signed cookies)
+# app.config["SESSION_FILE_DIR"] = mkdtemp()
+# app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_TYPE"] = "filesystem"
+# Session(app)
 
 # configure CS50 Library to use SQLite database
 # db = SQL("sqlite:///finance.db")
@@ -58,6 +57,9 @@ def index():
 
 
     return render_template("apology.html")
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
 
 # @app.route("/buy", methods=["GET", "POST"])
 # @login_required
